9ef76f8093921d549b957db78e7bafec3bdf281e
NavaPrashanth <prashanthnava1234@gmail.com>
2020-02-26 21:43:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tabs_updated/PageAdapter.java
@@ -0,0 +1,42 @@
+package com.example.tabs_updated;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PageAdapter extends FragmentStatePagerAdapter {
+    private int numberoftabs;
+
+    public PageAdapter(@NonNull FragmentManager fm,int numberoftabs) {
+        super(fm);
+        this.numberoftabs=numberoftabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new tab1();
+            case 1:
+                return new tab2();
+            case 2:
+                return new tab3();
+            case 3:
+                return new tab4();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numberoftabs;
+    }
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return POSITION_NONE;
+    }
+}
